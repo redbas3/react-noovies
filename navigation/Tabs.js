180a33876ee7d5841865549abb5037dce8c8a281
RJTmini <redbas3@gmail.com>
2023-06-29 11:50:26 +0900

    #3.4

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -15,6 +15,9 @@ const Tabs = () => {
   const isDark = useColorScheme() === "dark";
   return (
     <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: isDark ? BLACK_COLOR : "white",
+      }}
       screenOptions={{
         tabBarStyle: {
           backgroundColor: isDark ? BLACK_COLOR : "white",
--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,47 +1,132 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Dimensions,
+  StyleSheet,
+  useColorScheme,
+} from "react-native";
 import styled from "styled-components/native";
+import Swiper from "react-native-swiper";
+import { makeImgPath } from "../utils";
+import { BlurView } from "expo-blur";
 
-const Btn = styled.TouchableOpacity`
+const API_KEY = "0982b256ea73339e86ad4ea71063ef1f";
+
+const Container = styled.ScrollView``;
+
+const View = styled.View`
+  flex: 1;
+`;
+
+const Loader = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: ${(props) => props.theme.mainBgColor};
 `;
 
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor};
+const BgImg = styled.Image``;
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Container = styled.View`
-  flex: 1;
+const Poster = styled.Image`
+  width: 100px;
+  height: 160px;
+  border-radius: 5px;
 `;
 
-const Header = styled.View`
-  flex: 1;
-  background-color: red;
-`;
 const Column = styled.View`
-  flex: 1;
-  background-color: tomato;
-`;
-const Footer = styled.View`
-  flex: 1;
-  background-color: blue;
+  width: 40%;
+  margin-left: 15px;
 `;
 
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
-  navigation: { navigate },
-}) => (
-  <Container>
-    <Header></Header>
-    <Column>
-      <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
-        <Title>Movies!</Title>
-      </Btn>
-    </Column>
-    <Footer></Footer>
-  </Container>
-);
+const Title = styled.Text<{ isDark: boolean }>`
+  font-size: 16px;
+  font-weight: 600;
+  color: ${(props) => (props.isDark ? "white" : props.theme.textColor)};
+`;
+
+const Overview = styled.Text<{ isDark: boolean }>`
+  margin-top: 10px;
+  color: ${(props) =>
+    props.isDark ? "rgba(255, 255, 255, 0.8)" : "rgba(0, 0, 0, 0.8)"};
+`;
+
+const Votes = styled(Overview)`
+  margin-top: 5px;
+  font-size: 12px;
+`;
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const isDark = useColorScheme() === "dark";
+  const [loading, setLoading] = useState(true);
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const getNowPlaying = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&region=KR&api_key=${API_KEY}`
+    );
+    const { results } = await response.json();
+    setNowPlayingMovies(results);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getNowPlaying();
+  }, []);
+  return loading ? (
+    <Loader>
+      <ActivityIndicator size="small"></ActivityIndicator>
+    </Loader>
+  ) : (
+    <Container>
+      <Swiper
+        horizontal
+        loop
+        autoplay
+        autoplayTimeout={3.5}
+        showsButtons={false}
+        showsPagination={false}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 3 }}
+      >
+        {nowPlayingMovies.map((movie) => (
+          <View key={movie.id}>
+            <BgImg
+              source={{ uri: makeImgPath(movie.backdrop_path) }}
+              style={StyleSheet.absoluteFill}
+            ></BgImg>
+            <BlurView
+              intensity={85}
+              tint={isDark ? "dark" : "light"}
+              style={StyleSheet.absoluteFill}
+            >
+              <Wrapper>
+                <Poster
+                  source={{ uri: makeImgPath(movie.poster_path) }}
+                ></Poster>
+                <Column>
+                  <Title isDark={isDark}>{movie.original_title}</Title>
+                  {movie.vote_average > 0 ? (
+                    <Votes isDark={isDark}>⭐ {movie.vote_average}/10</Votes>
+                  ) : null}
+                  <Overview isDark={isDark}>
+                    {movie.overview.length > 80
+                      ? movie.overview.slice(0, 80) + "..."
+                      : movie.overview}
+                  </Overview>
+                </Column>
+              </Wrapper>
+            </BlurView>
+          </View>
+        ))}
+      </Swiper>
+    </Container>
+  );
+};
 
 export default Movies;
